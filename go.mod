a35455c34cb340a581acf228e2f2027068a4e86c
dingjing <dingjing@live.cn>
2025-08-11 17:22:34 +0800

    [ADD] fuse2 例子

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-fuse2-example
+
+go 1.24
+
+require (
+	github.com/hanwen/go-fuse/v2 v2.8.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path"
+	"runtime/pprof"
+	"syscall"
+	"time"
+
+	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+)
+
+func writeMemProfile(fn string, sigs <-chan os.Signal) {
+	i := 0
+	for range sigs {
+		fn := fmt.Sprintf("%s-%d.memprof", fn, i)
+		i++
+
+		log.Printf("Writing mem profile to %s\n", fn)
+		f, err := os.Create(fn)
+		if err != nil {
+			log.Printf("Create: %v", err)
+			continue
+		}
+		pprof.WriteHeapProfile(f)
+		if err := f.Close(); err != nil {
+			log.Printf("close %v", err)
+		}
+	}
+}
+
+func main() {
+	log.SetFlags(log.Lmicroseconds)
+
+	// Scans the arg list and sets up flags
+	directmount := flag.Bool("directmount", false, "try to call the mount syscall instead of executing fusermount")
+	directmountstrict := flag.Bool("directmountstrict", false, "like directmount, but don't fall back to fusermount")
+
+	flag.Parse()
+	if flag.NArg() < 2 {
+		fmt.Printf("usage: %s MOUNTPOINT ORIGINAL\n", path.Base(os.Args[0]))
+		fmt.Printf("\noptions:\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	orig := flag.Arg(1)
+	loopbackRoot, err := fs.NewLoopbackRoot(orig)
+	if err != nil {
+		log.Fatalf("NewLoopbackRoot(%s): %v\n", orig, err)
+	}
+
+	sec := time.Second
+	opts := &fs.Options{
+		// The timeout options are to be compatible with libfuse defaults,
+		// making benchmarking easier.
+		AttrTimeout:  &sec,
+		EntryTimeout: &sec,
+
+		NullPermissions: true, // Leave file permissions on "000" files as-is
+
+		MountOptions: fuse.MountOptions{
+			AllowOther:        true,
+			Debug:             true,
+			DirectMount:       *directmount,
+			DirectMountStrict: *directmountstrict,
+			FsName:            orig,       // First column in "df -T": original dir
+			Name:              "loopback", // Second column in "df -T" will be shown as "fuse." + Name
+		},
+	}
+	if opts.AllowOther {
+		// Make the kernel check file permissions for us
+		opts.MountOptions.Options = append(opts.MountOptions.Options, "default_permissions")
+	}
+	opts.MountOptions.Options = append(opts.MountOptions.Options, "ro")
+	server, err := fs.Mount(flag.Arg(0), loopbackRoot, opts)
+	if err != nil {
+		log.Fatalf("Mount fail: %v\n", err)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		server.Unmount()
+	}()
+
+	server.Wait()
+}
